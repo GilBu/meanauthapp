c46a1d3de01e44def7069a9f7f8ea5705749c0a2
GilBu <solknightx@gmail.com>
2018-03-05 09:36:11 -0800

    added navbar and register page

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import {Http, Headers, Response, Request} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+@Injectable()
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  email: String;
+  username: String;
+  password: String;
+
+  onSubmit(f: NgForm) {
+    console.log(this.name);
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    this.http.post('http://localhost:3000/users/register', JSON.stringify(user),{ headers: headers});
+    console.log(JSON.stringify(user));
+  }
+  constructor(
+    private http:Http,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
